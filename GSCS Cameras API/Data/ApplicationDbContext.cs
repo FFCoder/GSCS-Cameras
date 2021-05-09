2dbca53e596e41cf72fb2bea795092229cbf4c1c
admin.jonathon <admin.jonathon@gscs.org>
2021-05-08 23:25:13 -0400

    Add project files.

---
+++ GSCS Cameras API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSCS_Cameras_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<School> Schools { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
