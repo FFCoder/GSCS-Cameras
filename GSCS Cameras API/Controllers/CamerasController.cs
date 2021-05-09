e3a00ba5d773c4db96ff3871d1ab8c2d17d31c11
Jonathon Chambers <jonathon@jonathonchambers.com>
2021-05-09 00:36:46 -0400

    Added CamerasController

---
+++ GSCS Cameras API/Controllers/CamerasController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GSCS_Cameras_API.Data;
+
+namespace GSCS_Cameras_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CamerasController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+        
+
+        public CamerasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cameras
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Camera>>> GetCameras()
+        {
+            return await _context.Cameras.ToListAsync();
+        }
+
+        // GET: api/Cameras/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Camera>> GetCamera(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camera = await _context.Cameras
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (camera == null)
+            {
+                return NotFound();
+            }
+
+            return camera;
+        }
+
+       [HttpPost]
+        public async Task<ActionResult<Camera>> PostCamera(Camera camera)
+        {
+            _context.Cameras.Add(camera);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCamera", new { id = camera.ID }, camera);
+        }
+
+        private bool CameraExists(int id)
+        {
+            return _context.Cameras.Any(e => e.ID == id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCamera(int id, Camera camera)
+        {
+            if (id != camera.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(camera).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CameraExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Cameras/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCamera(int id)
+        {
+            var camera = await _context.Cameras.FindAsync(id);
+            if (camera == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cameras.Remove(camera);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ GSCS Cameras API/Migrations/20210509042308_Camera_mig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GSCS_Cameras_API.Migrations
+{
+    public partial class Camera_mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
